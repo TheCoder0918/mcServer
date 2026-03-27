a4d9945e878dc6ab78999374f112ce32af11894f
TheCoder0918 <thelordofnight.sebi@gmail.com>
2026-03-27 16:17:27 +0200

    init

---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:start
+java -Xms3072M -Xmx3072M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
+
+:git_save
+:: get the current date and time
+for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
+set timestamp=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%
+
+:: git add, commit, and push
+git add .
+git commit -m "save (%timestamp%)"
+git push -u origin main
+
+echo Server restarting...
+goto start
+echo: 
+echo Press CTRL + C to stop.
+goto :git_save
